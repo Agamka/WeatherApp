df5b105bb7ea843c8aeffa72f84d966ef24e6b69
Chlod Agnieszka <agnieszka.chlod@edu.fh-joanneum.at>
2021-11-16 11:08:03 +0100

    edit api

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//let apiKey = f9f7f81eb5386ea3ae64a04a9cf5b75b;
+
    // let city = London;
 
 function loadData(city) {
     
 
-    fetch("api.openweathermap.org/data/2.5/weather?q=London&appid=f9f7f81eb5386ea3ae64a04a9cf5b75b")
+    fetch("")
         .then(response => console.log(response.json()));
             
           /*  {
